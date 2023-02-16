4dca808642f6cc01ca30f4e8fb235386b0e02034
n0xON <92261947+n0xONwithPy@users.noreply.github.com>
2023-02-16 17:33:58 -0300

    Layout n dynamic objects in production

--- scripts/reveal_itens.js
+++ scripts/reveal_itens.js
@@ -5,12 +5,12 @@ selectElement.addEventListener('change', function(event) {
   const selectedOption = event.target.value;
 
   if (selectedOption === 'search_box_excel') {
-    // esconde a seção
+    
     detailItens.style.display = 'none';
   } else {
-    // exibe a seção
+    
     detailItens.style.display = 'block';
-    // altera o conteúdo da seção de acordo com a opção selecionada
+  
     detailItens.querySelector('.primary_text_title').textContent = `Descrição da Empresa: ${selectedOption}`;
     detailItens.querySelector('.primary_image img').alt = `Descrição do arquivo ${selectedOption}`;
   }
